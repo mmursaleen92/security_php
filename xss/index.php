225e336f8b8e92314d58f4418953f77c8939d31c
mmursaleen92 <mmursaleen92@gmail.com>
2018-09-29 22:11:12 +0500

    update file

--- xss/index.php
+++ xss/index.php
@@ -1,4 +1,8 @@
 <?php require "../db_connection.php" ?>
+<?php
+    $commentObject = $conn->query("SELECT * FROM comment");
+    $commentObject -> setfetchMode(PDO::FETCH_OBJ);
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,7 +22,42 @@
    </textarea><br /><br />
   
    <input type="submit" name="done">
+   
    </form>
+   <table>
+   <thead>
+   <tr>
+   <th>Name</th>
+   <th>Comment</th>
+   </tr>
+   </thead>
+   <tbody>
+      <tr>
+      <td>
+      <?php
+    //   while($comment = $commentObject->fetch())
+    //   {
+
+    //      echo "$comment->name <br />";
+    //     // echo "$comment->comment <br />";
+
+    //   }
+         ?>
+         </td>
+         <td>
+         <?php
+         while($comment = $commentObject->fetch())
+         {
+
+         
+            echo "$comment->comment <br />";
+        }
+         ?>
+         </td>
+         
+      </tr>
+   </tbody>
+   </table>
    
 </body>
 </html>
@@ -26,13 +65,11 @@
 
    if(isset($_POST['done']))
    {
-   	$name = $_POST['name'];
-   	$comment = $_POST['comment'];
+   	$name = trim(strip_tags($_POST['name']));
+   	$comment = mysqli_real_escape_string($conn,$_POST['comment']);
 
    	   $query = $conn->prepare("INSERT INTO comment(name,comment) VALUES(:name,:comment)");
    	   // $query .="VALUES (:name,:comment)";
    	   $query->execute(['name' => $name, 'comment' => $comment]);
 
-
-
-   }
+ }
